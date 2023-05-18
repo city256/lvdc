fdcc85e832a556670e549c1d0b0777e2299ee9c2
city256 <city256@naver.com>
2023-05-18 18:02:45 +0900

    테스트 코드 연습장

--- csv_data.py
+++ csv_data.py
@@ -1,11 +1,27 @@
+import string
+
 import pandas as pd
 import numpy as np
 import csv
+import re
 
 
 
-data = pd.read_csv('test.csv')
+dataset = pd.read_csv('load_train.csv', encoding='CP949')
 
-print(data.loc[1])
+
+print(dataset)
+print(dataset.values[0])
+data = pd.DataFrame(columns=['date','load'])
+index = 0
+
+for year, day, hour, load1, load2 in dataset.values:
+    nday = re.sub(r'[^0-9]', '',day)
+    nhour = re.sub(r'[^0-9]', '',hour)
+    data.loc[index] = [str(year)+nday.zfill(2)+nhour.zfill(2),load2]
+    index +=1
+#data = pd.DataFrame(columns=['date','load'])
+
+print(data)
 
 
