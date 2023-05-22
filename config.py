eaeaadd061b95e557651041620cb2d282f16644d
city256 <city256@naver.com>
2023-05-22 20:19:24 +0900

    soc에 따른 p_ref값 계산식 추가

--- config.py
+++ config.py
@@ -14,6 +14,32 @@ db_port = 13306
 
 # microgrid spec
 ess_capacity = 800
-converter_capacity = 250
+conv_capacity_1m = 250
 pv_capacity = 250
+conv_capacity_15s = 62.5
 
+#soc, target_soc = 0
+soc = 0.4
+target_soc = 0.34
+
+
+# ess 충방전, 목표 soc에 따른 power_reference 계산 (15분 단위)
+ess_energy = ess_capacity * (target_soc - soc) # 양수는 충전필요량, 음수는 방전필요량
+ess_energy = round(ess_energy, 2)
+ess_time = ess_energy / (conv_capacity_1m * 0.25) # 15분 주기로 충방전량 나누기
+
+print(ess_time, ess_energy)
+if(ess_time > 0): #양수, 충전
+    if(ess_time > 1):
+        p_ref = conv_capacity_15s
+    else:
+        p_ref = conv_capacity_15s * ess_time
+elif(ess_time < 0): #음수, 방전
+    if(ess_time < -1):
+        p_ref = -conv_capacity_15s
+    else:
+        p_ref = conv_capacity_15s * ess_time
+else: # 충방전 안함
+    p_ref = 0
+
+print(p_ref)
